0383e643087ae9d3b0920c6528852842e9b3f48d
yamamoto-hirofumi <sun.lionking.88@gmail.com>
2021-08-04 05:15:35 +0000

    [update]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,10 +1,11 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    @comment = current_user.book_comments.new(book_comment_params)
-    @comment.book_id = book.id
+    @book = Book.find(params[:book_id])
+    @comment = BookComment.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.user_id = current_user.id
     if @comment.save
-       redirect_to request.referer
+       render :create
     else
       @book = Book.find(params[:book_id])
       @book_new = Book.new
@@ -13,8 +14,9 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
-     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to request.referer
+     @book = Book.find(params[:book_id])
+     book_comment = @book.book_comments.find(params[:id])
+     book_comment.destroy
   end
 
   private
